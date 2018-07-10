332f4256483495764919e04f08c27673e7cf0054
Jonathan van Zuijlekom <jvz@bridgevest.com>
2018-07-10 14:02:49 +0200

    Upped mongodb driver to version 3.8

--- source/java/src/org/lucee/mongodb/MongoDBId.java
+++ source/java/src/org/lucee/mongodb/MongoDBId.java
@@ -4,17 +4,17 @@
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either 
+ * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public 
+ *
+ * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  **/
 package org.lucee.mongodb;
 
@@ -30,7 +30,7 @@ import org.bson.types.ObjectId;
 public class MongoDBId implements Function {
 
 	private static final long serialVersionUID = 2766144594043935912L;
-	
+
 	public static Object call(PageContext pc) throws PageException {
 		return call(pc, null);
 	}
@@ -47,7 +47,7 @@ public class MongoDBId implements Function {
 			if (!newID.isValid(initArgString)){
 				throw CFMLEngineFactory.getInstance().getExceptionUtil().createApplicationException("String ["+initArgString+"] passed as MongoDbId argument is not a valid ObjectID string");
 			}
-			newID = new ObjectId( initArgString );	
+			newID = new ObjectId( initArgString );
 		}
 		return new ObjectIdImpl( newID );
 	}
